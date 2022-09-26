ea75383ffe9ef9e750e45ba7ed945da36705b21d
xearonet <xearonet@o2.pl>
2022-09-26 15:26:53 +0200

    Init

---
+++ fix-apt-keys.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+# fix missing APT keys
+
+
+if [ `id -u` -ne 0 ]; then
+    echo "You must be ROOT to run this script"
+    exit 1
+fi
+
+echo -n "Updating the package list (may take a while)... " && \
+  apt-get update >/dev/null 2>/tmp/apt-get-update-errors && echo "OK"
+
+if ! grep -q NO_PUBKEY /tmp/apt-get-update-errors; then
+    echo "All the keys are ok, nothing to do."
+    rm -f /tmp/apt-get-update-errors
+    exit 0
+fi
+
+for KEY in `awk '/NO_PUBKEY/ {print $NF}' /tmp/apt-get-update-errors`; do
+    echo "Processing $KEY:"
+    echo -n " -> getting the key from the server... " && \
+      gpg --keyserver pgp.mit.edu --recv-keys "$KEY" >/dev/null 2>&1 && \
+      echo "OK" && echo -n " -> adding a key to the database... " && \
+      gpg --armor --export "$KEY" | apt-key add -
+done
+rm -f /tmp/apt-get-update-errors
